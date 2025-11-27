6cdd0d824da0a034039e73908e8cbfe4303fdb73
azizt91 <21195038@mhs.stmik-tegal.ac.id>
2025-11-27 10:45:08 +0700

    fix status bar

--- lib/core/widgets/customer_scaffold.dart
+++ lib/core/widgets/customer_scaffold.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import '../theme/app_colors.dart';
 
@@ -31,7 +32,13 @@ class _CustomerScaffoldState extends State<CustomerScaffold> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.light, // For iOS
+      ),
+      child: Scaffold(
       key: _scaffoldKey,
       body: widget.child,
       bottomNavigationBar: BottomNavigationBar(
@@ -75,6 +82,7 @@ class _CustomerScaffoldState extends State<CustomerScaffold> {
             label: 'Profile',
           ),
         ],
+      ),
       ),
     );
   }

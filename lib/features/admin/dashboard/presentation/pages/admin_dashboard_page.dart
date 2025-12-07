d9fe9fb835c4aa10263a68ed52bd724f251b79e9
azizt91 <21195038@mhs.stmik-tegal.ac.id>
2025-12-07 19:09:06 +0700

    fix tampilan dashboard masih ada space

--- lib/features/admin/dashboard/presentation/pages/admin_dashboard_page.dart
+++ lib/features/admin/dashboard/presentation/pages/admin_dashboard_page.dart
@@ -123,7 +123,6 @@ class _AdminDashboardPageState extends ConsumerState<AdminDashboardPage> {
     final year = ref.read(selectedYearProvider);
     return Column(children: [
       _largeCard('Profit', f.format(s.profit), Icons.account_balance_wallet, [const Color(0xFF9969C7), const Color(0xFF6A359C)], _showProfit, () => setState(() => _showProfit = !_showProfit)),
-      const SizedBox(height: 8),
       GridView.count(crossAxisCount: 2, shrinkWrap: true, physics: const NeverScrollableScrollPhysics(), mainAxisSpacing: 8, crossAxisSpacing: 8, childAspectRatio: 1.2, children: [
         _smallToggle('Pendapatan', f.format(s.totalRevenue), Icons.trending_up, [const Color(0xFF004e92), const Color(0xFF000428)], _showPendapatan, () => setState(() => _showPendapatan = !_showPendapatan)),
         _smallToggle('Pengeluaran', f.format(s.totalExpenses), Icons.trending_down, [const Color(0xFF485563), const Color(0xFF29323c)], _showPengeluaran, () => setState(() => _showPengeluaran = !_showPengeluaran)),
